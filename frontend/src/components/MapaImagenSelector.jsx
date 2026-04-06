c951cefa6250275070cb7cd340cc39b849672a07
valeriamartin5230 <valeria.martin5203@alumnos.udg.mx>
2026-04-06 15:01:14 -0600

    arreglar problema

--- frontend/src/components/MapaImagenSelector.jsx
+++ frontend/src/components/MapaImagenSelector.jsx
@@ -200,7 +200,7 @@ function MapaImagenSelector({ ubicacionSeleccionada, onUbicacionChange, moduloSe
             >
               <img
                 ref={imagenRef}
-                src="/images/mapa-campus.png"
+                src="/Fondo/mapacucei.jpg"
                 alt="Mapa del campus - Haz clic para seleccionar ubicación"
                 className="mapa-imagen"
                 onClick={handleImagenClick}
